e91806d743740cb23b77654e537130b6a5d328a7
eden <edenmekonnen130@gmail.com>
2024-12-19 11:20:17 +0300

    user and product

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,12 +7,33 @@ const userSignInController = require('../controller/user/userSignIn')
 const userDetailsController = require('../controller/user/userDetails')
 const authToken = require('../middleware/authToken')
 const userLogout = require('../controller/user/userLogout')
+const userLogout = require('../controller/user/userLogout')
+const allUsers = require('../controller/user/allUsers')
+const updateUser = require('../controller/user/updateUser')
+const UploadProductController = require('../controller/product/uploadProduct')
+const getProductController = require('../controller/product/getProduct')
+const updateProductController = require('../controller/product/updateProduct')
+const searchProduct = require('../controller/product/searchProduct')
+const getProductDetails = require('../controller/product/getProductDetails')
+
 
 // user
 router.post("/signup",userSignUpController)
 router.post("/signin",userSignInController)
 router.get("/user-details",authToken,userDetailsController)
 router.get("/userLogout",userLogout)
+
+//admin panel 
+router.get("/all-user",authToken,allUsers)
+router.post("/update-user",authToken,updateUser)
+
+
+//product
+router.post("/upload-product",authToken,UploadProductController)
+router.get("/get-product",getProductController)
+router.post("/update-product",authToken,updateProductController)
+router.post("/product-details",getProductDetails)
+router.get("/search",searchProduct)
 
 
 module.exports = router
